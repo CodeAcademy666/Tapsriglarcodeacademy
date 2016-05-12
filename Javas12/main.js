1be94384bf17a848d17c1fd16545c347b3bdac04
haciyev666 <arif.h@code.edu.az>
2016-05-12 16:18:04 +0500

    Proyektlerim

---
+++ Javas12/main.js
@@ -0,0 +1,16 @@
+// var i=0;
+// (function efqanFunc() {
+//   i++;
+//   document.write("Arif <br>"+i);
+//   if (i<500) {
+//     efqanFunc();
+//   }
+// })();
+var obj-{
+  ad=:"Arif",
+  soyad:"Haciyev",
+  ekrandayaz:function(){
+   console.log(this.ad+" "+this.soyad);
+  }
+}
+obj.ekrandayaz();
