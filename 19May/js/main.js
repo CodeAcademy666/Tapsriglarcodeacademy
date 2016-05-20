eb52bb4f2d38e8340c8d88dbae2ea4f41358a773
haciyev666 <arif.h@code.edu.az>
2016-05-20 15:51:23 +0500

    20may

--- 19May/js/main.js
+++ 19May/js/main.js
@@ -1,5 +1,5 @@
-var obj2={
-	name:1,
-	surname:2,
-}
-console.log(obj2.__proto__)
+// var obj2={
+// 	name:1,
+// 	surname:2,
+// }
+// console.log(obj2.__proto__)
---
+++ 20May/js/main.js
@@ -0,0 +1,21 @@
+// document.getElementsByTagName("div")[0].style.background="aqua";
+// document.getElementsByTagName("div")[0].style.background="red";
+
+// document.getElementsByClassName("ibne")[0].style.background="#00695C";
+// document.getElementsByClassName("ibne")[0].style.height="90px";
+// document.getElementsByClassName("ibne")[0].style.width="70px";
+
+// document.getElementsByClassName("ibneOne")[0].style.background='#00695C';
+// document.getElementsByClassName("ibneOne")[0].style.height="100px";
+// document.getElementsByClassName("ibneOne")[0].style.width="100px";
+
+// document.getElementsByClassName("ibne2")[0].style.background="#2196F3";
+// document.getElementsByClassName("ibne2")[0].style.height="190px";
+// document.getElementsByClassName("ibne2")[0].style.width="500px";
+
+// document.getElementsByClassName("ibne3")[0].style.background="#004D40";
+// document.getElementsByClassName("ibne3")[0].style.height="100px";
+// document.getElementsByClassName("ibne3")[0].style.width="120px";
+
+
+
